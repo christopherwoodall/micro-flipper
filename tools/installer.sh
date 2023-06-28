69a4df9a30766a383be7a7fbe36d8bface3bcb48
Christopher Woodall <christopher.woodall@twosixtech.com>
2023-06-27 21:49:35 -0500

    Update installer.sh

--- tools/installer.sh
+++ tools/installer.sh
@@ -6,19 +6,15 @@
 
 repo_url="https://github.com/christopherwoodall/micro-flipper.git"
 
+
 # Create a temporary directory for cloning the repository
 tmp_dir=$(mktemp -d)
 
 git clone "$repo_url" "$tmp_dir"
-
 cd "$tmp_dir"
-
 git remote add upstream "$repo_url"
-
 git fetch upstream
-
 git reset upstream/main
-
 git checkout -t upstream/main
 
 rm -rf "$tmp_dir"
@@ -26,15 +22,8 @@ rm -rf "$tmp_dir"
 
 # Python Installer
 python -m venv venv
-
 source venv/bin/activate
-
-pip install PyYAML gitpython
-
-chmod +x setup.py
-
-./tools/setup.py
-
+pip install --editable .
 
 
 echo "Installation completed successfully."
